9da71c6914485f4cd4cfad15bb2702da8acea348
nao <nao@gotounoMacBook-Air.local>
2022-04-01 01:33:22 +0900

    modifed tailwind settings and creatd login UI

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,13 +1,23 @@
+import { Input } from '@chakra-ui/react'
+
 const Login = () => {
     return (
         <>
-            <div className="bg-dark-green">
-                <dl>
-                    <dt>E-mail</dt>
-                    <dd>
-                        <input type="text" />
-                    </dd>
-                </dl>
+            <div className="bg-[#f4f4f4] min-h-screen flex items-center">
+                <div className="max-w-2xl bg-white p-8 drop-shadow-md">
+                    <dl className="">
+                        <dt>E-mail</dt>
+                        <dd>
+                            <Input />
+                        </dd>
+                        <dt>Password</dt>
+                        <dd>
+                            <Input />
+                        </dd>
+                    </dl>
+                    <button >Login</button>
+
+                </div>
             </div>
         </>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,18 @@
 module.exports = {
   mode: "jit",
-  purge: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
-  content: [],
   theme: {
     extend: {
       colors: {
         'dark-green': '#84fab0',
         'dark-blue': '#8fd3f4'
       },
-    },
-    fontFamily: {
-      'body': ['"Roboto"', '"Noto Sans JP"'],
+      fontFamily: {
+        'body': ['"Roboto"', '"Noto Sans JP"'],
+      },
     },
 
   },
