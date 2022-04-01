4d5917f4f1ae0ae01be43d1dab23d25a10ede383
nao <nao@gotounoMacBook-Air.local>
2022-04-01 23:47:49 +0900

    modified login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,21 +1,20 @@
-import { Input } from '@chakra-ui/react'
-
 const Login = () => {
     return (
         <>
             <div className="bg-[#f4f4f4] min-h-screen flex items-center">
-                <div className="max-w-2xl bg-white p-8 drop-shadow-md">
-                    <dl className="">
-                        <dt>E-mail</dt>
-                        <dd>
-                            <Input />
+                <div className="min-w-[400px] bg-white p-8 drop-shadow-lg mx-auto w-1/4 ">
+                    <h2 className="text-2xl mb-[30px]">Login</h2>
+                    <dl>
+                        <dt className="mb-[10px]">E-mail</dt>
+                        <dd className="mb-[30px]">
+                            <input className="w-full border-b border-light-gray pb-1 focus:border-gradient-orange" />
                         </dd>
-                        <dt>Password</dt>
-                        <dd>
-                            <Input />
+                        <dt className="mb-[10px]">Password</dt>
+                        <dd className="mb-[40px]">
+                            <input className="w-full border-b border-light-gray pb-1 focus:border-gradient-orange" />
                         </dd>
                     </dl>
-                    <button >Login</button>
+                    <button className="bg-gradient-to-r from-gradient-red to-gradient-orange w-full text-white p-3 rounded-full">Enter</button>
 
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,10 @@ module.exports = {
     extend: {
       colors: {
         'dark-green': '#84fab0',
-        'dark-blue': '#8fd3f4'
+        'dark-blue': '#8fd3f4',
+        'gradient-red': '#ff5858',
+        'gradient-orange': '#f09819',
+        'light-gray': '#cccccc'
       },
       fontFamily: {
         'body': ['"Roboto"', '"Noto Sans JP"'],
